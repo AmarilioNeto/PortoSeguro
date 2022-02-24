841ee29939582c96274cf68693515d65a6dada7f
AmarilioNeto <80914219+AmarilioNeto@users.noreply.github.com>
2022-02-23 22:17:36 -0300

    Esta em C# c#
    tentando Conveter para VB

---
+++ Port Seguro API/Port Seguro API/UploadController.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Diagnostics
+Imports System.Net
+Imports System.Net.Http
+Imports System.Threading.Tasks
+Imports System.Web
+Imports System.Web.Http
+
+
+Public Class UploadController
+    Inherits ApiController
+
+    Public Async Function PostFormData() As Task(Of HttpResponseMessage)
+
+        Dim root As String = HttpContext.Current.Server.MapPath("~/App_Data")
+        Dim provider = New MultipartFormDataStreamProvider(root)
+
+        Try
+            Await Request.Content.ReadAsMultipartAsync(provider)
+
+            For Each file As MultipartFileData In provider.FileData
+                Trace.WriteLine(file.Headers.ContentDisposition.FileName)
+                Trace.WriteLine("Server file path: " & file.LocalFileName)
+            Next
+
+            Return Request.CreateResponse(HttpStatusCode.OK)
+        Catch e As System.Exception
+            Return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e)
+        End Try
+    End Function
+End Class
